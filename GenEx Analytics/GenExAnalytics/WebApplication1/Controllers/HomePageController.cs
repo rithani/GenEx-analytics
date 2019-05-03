821f68c33fa84159a482c249f5d718e5d85dee7b
Raji04 <43502891+rithani@users.noreply.github.com>
2019-05-02 21:22:48 -0700

    GenEx-Sample UI design

---
+++ GenEx Analytics/GenExAnalytics/WebApplication1/Controllers/HomePageController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult MonthEndManagement()
+        {
+            return View();
+        }
+        public ActionResult Earned()
+        {
+            return View();
+        }
+    }
+}
